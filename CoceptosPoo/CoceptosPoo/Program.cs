3ac40bd3eb753f15661b7530bacca223c0c1c8a0
CarlosMezai <mezaca85@gmail.com>
2022-08-05 19:29:07 -0500

    Correction main

--- CoceptosPoo/CoceptosPoo/Program.cs
+++ CoceptosPoo/CoceptosPoo/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-<<<<<<< Updated upstream
-=======
 using System.Collections.Generic;
->>>>>>> Stashed changes
-
 
 namespace CoceptosPoo
 {
@@ -16,63 +12,6 @@ namespace CoceptosPoo
 
             Employee employee1 = new SalaryEmployee()
             {
-                Id = 1001,
-                FirstName = "Silvia",
-                LastName = "Meza",
-                BirthDate = new Date(1995, 05, 26),
-                HiringDate = new Date(2020, 01, 01),
-                IsActive = true,
-                Salary = 1900000.50M,
-
-            };
-
-            Console.WriteLine(employee1);
-
-            Employee employee2 = new CommissionEmployee()
-            {
-                Id = 1002,
-                FirstName = "Jairo",
-                LastName = "Sierra",
-                BirthDate = new Date(1997, 07, 13),
-                HiringDate = new Date(2021, 01, 01),
-                IsActive = true,
-                Sales = 3200000M,
-                CommissionPercentaje = 0.03F,
-
-            };
-
-            Console.WriteLine(employee2);
-
-            Employee employee3 = new HouryEmployee()
-            {
-                Id = 1003,
-                FirstName = "Diego",
-                LastName = "Gonzalez",
-                BirthDate = new Date(1985, 05, 26),
-                HiringDate = new Date(2021, 01, 01),
-                IsActive = true,
-                HourValue = 40000.50M,
-                Hours = 123.5F
-
-            };
-
-            Console.WriteLine(employee3);
-            //Date date1 = new Date(2022, 08, 02);
-            //try
-            //{
-            //    Console.WriteLine(new Date(2024, 2, 29));
-            //    Console.WriteLine(new Date(1974, 11, 03));
-            //    Console.WriteLine(new Date(1985, 11, 31));
-            //}
-            //catch (Exception error)
-            //{
-            //    Console.WriteLine(error.Message);
-
-            //}
-               // Console.WriteLine(error.Message);
-             
-            }
-=======
                 Id = 1001,
                 FirstName = "Silvia",
                 LastName = "Meza",
@@ -113,7 +52,6 @@ namespace CoceptosPoo
 
             };
 
-            //Console.WriteLine(employee3);
 
             Employee employee4 = new BaseCommissionEmployee()
             {
@@ -179,8 +117,8 @@ namespace CoceptosPoo
             //{
             //    Console.WriteLine(error.Message);
 
-            //}
->>>>>>> Stashed changes
+            //
+            //
         }
     }
 }
